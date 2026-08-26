a0acf1a15b6f5d126e63791a1f221041b3ee32b9
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-26 04:57:19 -0400

    fix: use correct Button all-caps API

--- app/src/main/java/io/github/astromg01/chatoverlay/MainActivity.java
+++ app/src/main/java/io/github/astromg01/chatoverlay/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends Activity {
     private Button button(String text) {
         Button b = new Button(this);
         b.setText(text);
-        b.setTextAllCaps(false);
+        b.setAllCaps(false);
         b.setTextSize(15);
         return b;
     }
